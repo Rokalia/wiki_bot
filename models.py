4981330ccbfa974de781fd1ecb2fefad22494f1f
Rokalia <155267391+Rokalia@users.noreply.github.com>
2024-12-29 18:25:54 +0300

    Create models.py

---
+++ models.py
@@ -0,0 +1,19 @@
+from sentence_transformers import SentenceTransformer, CrossEncoder, util
+from transformers import pipeline
+
+def get_models
+  retriver = SentenceTransformer(
+      model_name_or_path="DiTy/bi-encoder-russian-msmarco",
+      cache_folder="./hf_cache",
+  )
+  
+  reranker = CrossEncoder(
+      model_name="DiTy/cross-encoder-russian-msmarco",
+      max_length=512
+  )
+  
+  qa = pipeline(
+     task='question-answering',
+     model='timpal0l/mdeberta-v3-base-squad2'
+  )
+  return retriver, reranker, qa
